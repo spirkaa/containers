b21c0474621e659dfe177012f2be6dfc7719505a
Ilya Pavlov <piv@devmem.ru>
2023-01-07 12:11:43 +0300

    setup gh actions

--- .github/scripts/published.sh
+++ .github/scripts/published.sh
@@ -6,31 +6,31 @@ STABLE="${3}"
 # TOKEN="${TOKEN}"
 
 if [[ -z "${STABLE}" || "${STABLE}" == false ]]; then
-    APP="${APP}-${CHANNEL}"
+  APP="${APP}-${CHANNEL}"
 fi
 
-tags=$( \
-    curl -fsSL \
-        -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: token ${TOKEN}" \
-        "https://api.github.com/users/onedr0p/packages/container/${APP}/versions" \
-        2>/dev/null
+tags=$(
+  curl -fsSL \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token ${TOKEN}" \
+    "https://api.github.com/users/spirkaa/packages/container/${APP}/versions" \
+    2>/dev/null
 )
 
 if [[ -z "${tags}" ]]; then
-    exit 0
+  exit 0
 fi
 
-current_tags=$( \
-    jq --compact-output \
-        'map( select( .metadata.container.tags[] | contains("rolling") ) | .metadata.container.tags[] )' \
-            <<< "${tags}" \
+current_tags=$(
+  jq --compact-output \
+    'map( select( .metadata.container.tags[] | contains("rolling") ) | .metadata.container.tags[] )' \
+    <<<"${tags}"
 )
 
-tag=$( \
-    jq --compact-output \
-        'map( select( index("rolling") | not ) )' \
-            <<< "${current_tags}"
+tag=$(
+  jq --compact-output \
+    'map( select( index("rolling") | not ) )' \
+    <<<"${current_tags}"
 )
 
-printf "%s" "$(jq --raw-output '.[0]' <<< "${tag}")"
+printf "%s" "$(jq --raw-output '.[0]' <<<"${tag}")"
